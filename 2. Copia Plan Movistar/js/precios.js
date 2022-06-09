97cacd06930f8b4bf189a16c440b8f92926bdd05
Noraklav <agustin.dv.2021@gmail.com>
2022-06-09 02:08:18 -0300

    Some changes and a new project created

---
+++ 2. Copia Plan Movistar/js/precios.js
@@ -0,0 +1,9 @@
+const seleccionar = str => document.querySelector(str)
+
+const dataPlans = [
+  {name: 'noEsMovistar', price: 2.199},
+  {name: 'esMovistar', price: 1.799},
+]
+
+const noUserPrice = seleccionar('#js__no-user')
+noUserPrice.innerHTML = dataPlans[0].price
