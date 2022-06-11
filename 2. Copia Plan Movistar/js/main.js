080336d07bab6ad40e125f19c5b00b872849e20d
Noraklav <agustin.dv.2021@gmail.com>
2022-06-11 02:44:36 -0300

    Muchos cambios

---
+++ 2. Copia Plan Movistar/js/main.js
@@ -0,0 +1,2 @@
+const btnWhatsapp = document.querySelector('#whatsapp__redirection')
+btnWhatsapp.onclick = () => window.open('https://api.whatsapp.com/message/PTV3EYQGKTMPK1?autoload=1&app_absent=0')
--- 2. Copia Plan Movistar/tab1/js/precios.js
+++
@@ -1,9 +0,0 @@
-const seleccionar = str => document.querySelector(str)
-
-const dataPlans = [
-  {name: 'noEsMovistar', price: 2.199},
-  {name: 'esMovistar', price: 1.799},
-]
-
-let userPrice = seleccionar('#js__is-user')
-userPrice.innerHTML = dataPlans[1].price
