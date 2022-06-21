a2f9c24dc2098530af3cd8c6510fdaa3656cdea2
Noraklav <agustin.dv.2021@gmail.com>
2022-06-21 12:37:01 -0300

    Card terminada
    Card de planes TV terminada
    Cambios al estilo del boton de whatsapp
    Falta organizar las card

--- 2. Copia Plan Movistar/js/esCliente.js
+++ 2. Copia Plan Movistar/js/esCliente.js
@@ -5,23 +5,35 @@ let userPrice = document.querySelector('#js__no-user')
 userPrice.innerHTML = planPrice
 
 // Dropdown card plan TV
-let dropdown = document.querySelector('.dropdown')
-dropdown.onclick = () =>{dropdown.classList.toggle('active')}
+let dropdown = document.querySelectorAll('.dropdown')
+dropdown.forEach(e => e.onclick = () => e.classList.toggle('active'))
 
 
-class PlanFibra{
-  constructor(megas, precioMegas, precioMBViejo){
-    this.megas = megas
-    this.precioMegas = String(precioMegas)
-    this.precioMBViejo = String(precioMBViejo)
+const planes = [
+  {id: 0, megas: 100, precioMegas: 2.199, precioViejo: 4.199},
+  {id: 1, megas: 300, precioMegas: 2.899, precioViejo: 4.899},
+  {id: 2, megas: 500, precioMegas: 3.699, precioViejo: 5.699},
+  {id: 3, megas: 'Giga', precioMegas: 5.199, precioViejo: 7.199}
+]
+
+let planMegas = document.querySelectorAll('.js__plan-gigas')
+let megasPrecio = document.querySelectorAll('.js__plan-precio')
+let megasPrecioViejo = document.querySelectorAll('.js__plan-precio-viejo')
+
+
+const printData = (array, option) =>{
+  let len = array.length
+  if(option == 1) {
+    for(let x=0;x<len;x++) array[x].innerHTML = planes[x].megas
+  }
+  else if(option == 2) {
+    for(let x=0;x<len;x++) array[x].innerHTML = planes[x].precioMegas
+  }
+  else if(option == 3) {
+    for(let x=0;x<len;x++) array[x].innerHTML = planes[x].precioViejo
   }
 }
 
-let mb_100 = new PlanFibra(100, 2.899, 4.899)
-
-const planes_container = document.getElementById('#planes__div')
-
-
-let h1 = document.createElement('h1').innerHTML = 'Tratando de agregar las card dinamicamente (╯°□°)╯︵ ┻━┻'
-
-document.body.append(h1)
+printData(planMegas, 1)
+printData(megasPrecio, 2)
+printData(megasPrecioViejo, 3)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Bootstrap-4-Proyects
+
+[gist de la card](https://gist.github.com/Noraklav/9d8be64c88074ff7f0ba2d0ae01f44bd)
